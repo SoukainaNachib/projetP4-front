ad54108ceee303699b3e06a3a9fbdc64ce4fe811
Nachib soukaina <soukaina.nachib23@gmail.com>
2020-05-09 14:30:43 +0100

    projetP4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
+import { InfermierComponent } from './components/infermier/infermier.component';
+import { MedcinComponent } from './components/medcin/medcin.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  { path: "", redirectTo: '/products', pathMatch: 'full' },
+  { path: "medcin", component: MedcinComponent },
+  { path: "infermier", component: InfermierComponent }
+  // { path:"**", component: PageNotFoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
