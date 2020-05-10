f6ba7d0fac523bac134d0d9f817356588151af34
Nachib soukaina <soukaina.nachib23@gmail.com>
2020-05-10 01:12:28 +0100

    router-link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,18 @@
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { FormulaireComponent } from './components/formulaire/formulaire.component';
+import { ReceptionisteComponent } from './components/receptioniste/receptioniste.component';
+import { UserComponent } from './components/user/user.component';
+import { SyntheseComponent } from './components/synthese/synthese.component';
+import { SpecialiteComponent } from './components/specialite/specialite.component';
+import { RoleComponent } from './components/role/role.component';
+import { RendezVousComponent } from './components/rendez-vous/rendez-vous.component';
+import { PracticienComponent } from './components/practicien/practicien.component';
+import { PatientComponent } from './components/patient/patient.component';
+import { DossierMComponent } from './components/dossier-m/dossier-m.component';
+import { CommunicationComponent } from './components/communication/communication.component';
+import { AutreResourceComponent } from './components/autre-resource/autre-resource.component';
+import { AdministrationComponent } from './components/administration/administration.component';
+import { AuthComponent } from './components/auth/auth.component';
 import { InfermierComponent } from './components/infermier/infermier.component';
 import { MedcinComponent } from './components/medcin/medcin.component';
 import { NgModule } from '@angular/core';
@@ -7,10 +22,23 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 
-  { path: "", redirectTo: '/products', pathMatch: 'full' },
+  { path: "auth", component: AuthComponent },
+  { path: "administration", component: AdministrationComponent },
+  { path: "autre-resource", component: AutreResourceComponent },
+  { path: "communication", component: CommunicationComponent },
+  { path: "dossier-m", component: DossierMComponent },
+  { path: "patient", component: PatientComponent },
+  { path: "practicien", component: PracticienComponent },
+  { path: "rendez-vous", component: RendezVousComponent },
+  { path: "role", component: RoleComponent },
+  { path: "specialite", component: SpecialiteComponent },
+  { path: "synthese", component: SyntheseComponent },
+  { path: "user", component: UserComponent },
+  { path: "receptioniste", component: ReceptionisteComponent },
   { path: "medcin", component: MedcinComponent },
-  { path: "infermier", component: InfermierComponent }
-  // { path:"**", component: PageNotFoundComponent }
+  { path: "infermier", component: InfermierComponent },
+  { path: "formulaire", component: FormulaireComponent },
+  { path: "**", component: PageNotFoundComponent },
 
 ];
 
@@ -19,3 +47,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [MedcinComponent, InfermierComponent, FormulaireComponent, AuthComponent, ReceptionisteComponent, PageNotFoundComponent, AdministrationComponent, AutreResourceComponent, CommunicationComponent, RoleComponent, UserComponent, SyntheseComponent, SpecialiteComponent, RendezVousComponent, PracticienComponent, PatientComponent, DossierMComponent] 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdministrationComponent } from './components/administration/administration.component';
-import { AutreResourceComponent } from './components/autre-resource/autre-resource.component';
-import { CommunicationComponent } from './components/communication/communication.component';
-import { DossierMComponent } from './components/dossier-m/dossier-m.component';
-import { InfermierComponent } from './components/infermier/infermier.component';
-import { MedcinComponent } from './components/medcin/medcin.component';
-import { PatientComponent } from './components/patient/patient.component';
-import { PracticienComponent } from './components/practicien/practicien.component';
-import { ReceptionisteComponent } from './components/receptioniste/receptioniste.component';
-import { RendezVousComponent } from './components/rendez-vous/rendez-vous.component';
-import { RoleComponent } from './components/role/role.component';
-import { SpecialiteComponent } from './components/specialite/specialite.component';
-import { SyntheseComponent } from './components/synthese/synthese.component';
-import { UserComponent } from './components/user/user.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { AuthComponent } from './components/auth/auth.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { FormulaireComponent } from './components/formulaire/formulaire.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdministrationComponent,
-    AutreResourceComponent,
-    CommunicationComponent,
-    DossierMComponent,
-    InfermierComponent,
-    MedcinComponent,
-    PatientComponent,
-    PracticienComponent,
-    ReceptionisteComponent,
-    RendezVousComponent,
-    RoleComponent,
-    SpecialiteComponent,
-    SyntheseComponent,
-    UserComponent,
     NavbarComponent,
     FooterComponent,
-    AuthComponent,
-    PageNotFoundComponent,
-    FormulaireComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
